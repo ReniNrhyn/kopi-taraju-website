32675a3c250f4ca9094b50be60246113f6b11a8e
ReniNrhyn <nurhayanireni73@gmail.com>
2025-03-01 15:22:31 +0700

    Update navbar dan sidebar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 // Toggle class active
 const navbarNav = document.querySelector(".navbar-nav");
-//ketika hamburger menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
+const hamburgerMenu = document.querySelector("#hamburger-menu");
+
+// Cek apakah elemen ditemukan
+console.log(navbarNav, hamburgerMenu);
+
+hamburgerMenu.onclick = () => {
   navbarNav.classList.toggle("active");
+  console.log("Hamburger diklik!");
 };
+
+//klik di luar sidebar untuk menghilangkan nav
+const Hamburger = document.querySelector("#hamburger=menu");
+
+document.addEventListener("click", function (e) {
+  if (!Hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
